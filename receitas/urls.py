79cf4cb1fa6e9aba67ef24b729e935056e150a97
BrenoOsvaldoFunicheli <osvaldobreno99@gmail.com>
2022-04-14 20:05:14 -0300

    refactor

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,11 +2,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import busca,receita, cria_receita, deleta_receita, edita_receita, atualiza_receita, index
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('<int:id_receita>/', views.receita, name="receita"),
-    path("buscar", views.buscar, name="buscar"),
-    path('request/', views.http_request, name="request"),
+    path('', index, name='index'),
+    path('<int:receita_id>', receita, name='receita'),
+    path('buscar', busca, name='buscar'),
+    path('cria/receita', cria_receita, name='criar_receita'),
+    path('deleta/<int:receita_id>',deleta_receita, name='deleta_receita'),
+    path('edita/<int:receita_id>',edita_receita, name='editar_receita'),
+    path('atualiza_receita', atualiza_receita, name='atualizar_receita')
 ]
+
